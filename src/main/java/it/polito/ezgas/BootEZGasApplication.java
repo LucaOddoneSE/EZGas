d778bb92270ed29681dc17d3aa7381ed2ab2b032
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-15 00:53:38 +0200

    docs:creating user admin if it doesn't exist and list all users(if any) present in database

--- src/main/java/it/polito/ezgas/BootEZGasApplication.java
+++ src/main/java/it/polito/ezgas/BootEZGasApplication.java
@@ -3,21 +3,37 @@ package it.polito.ezgas;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.polito.ezgas.controller.UserController;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+import it.polito.ezgas.repository.UserRepository;
+
 @SpringBootApplication
 public class BootEZGasApplication {
-
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+    private UserController userController;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootEZGasApplication.class, args);
 	}
 	
 	@PostConstruct
 	public void setupDbWithData() throws SQLException{
+		
+		List<UserDto> listUsers = new ArrayList<>();
+		Iterator<UserDto> iter;
 		
 		Connection conn = DriverManager.getConnection("jdbc:h2:./data/memo", "sa", "password");
 		conn.close();
@@ -26,14 +42,29 @@ public class BootEZGasApplication {
 		/*
 		 
 		list all the users stored in the database and, if there is no an admin user create it
-		 
-			User user= new User("admin", "admin", "admin@ezgas.com", 5);
-			user.setAdmin(true);
-			
 		and then save it in the db
 	
-			
 		*/
+		
+		System.out.println("All users stored in the database:\n");
+		if(userController.getAllUsers().size() != 0) {
+			System.out.println(userController.getAllUsers().toString());
+			listUsers = userController.getAllUsers();
+			iter = listUsers.iterator();
+			while(iter.hasNext()) {
+				if (iter.next().getAdmin() == true)
+					break;
+			}
+			User user= new User("admin", "admin", "admin@ezgas.com", 5);
+		    user.setAdmin(true);
+		    userRepository.save(user);
+		}
+		else {
+			System.out.println("None\n");
+			User user= new User("admin", "admin", "admin@ezgas.com", 5);
+		    user.setAdmin(true);
+		    userRepository.save(user);
+		}
 
 	}
 
