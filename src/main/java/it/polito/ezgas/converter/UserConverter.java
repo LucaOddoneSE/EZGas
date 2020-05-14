61d50c09f6bbca16d0e31320220c32b8fd50052a
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-15 00:33:54 +0200

    docs:adding converter package

---
+++ src/main/java/it/polito/ezgas/converter/UserConverter.java
@@ -0,0 +1,39 @@
+package it.polito.ezgas.converter;
+
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+
+public class UserConverter {
+	
+	private User user;
+	private UserDto userDto;
+	
+	public UserConverter() {
+		
+	}
+	
+	public UserDto toUserDto(User userSource) {
+		userDto = new UserDto();
+		userDto.setUserId(userSource.getUserId());
+		userDto.setUserName(userSource.getUserName());
+		userDto.setPassword(userSource.getPassword());
+		userDto.setEmail(userSource.getEmail());
+		userDto.setReputation(userSource.getReputation());
+		userDto.setAdmin(userSource.getAdmin());
+		
+		return userDto;
+	}
+	
+	public User toUser(UserDto userDtoSource) {
+		user = new User();
+		user.setUserId(userDtoSource.getUserId());
+		user.setUserName(userDtoSource.getUserName());
+		user.setPassword(userDtoSource.getPassword());
+		user.setEmail(userDtoSource.getEmail());
+		user.setReputation(userDtoSource.getReputation());
+		user.setAdmin(userDtoSource.getAdmin());
+		
+		return user;
+	}
+	
+}
