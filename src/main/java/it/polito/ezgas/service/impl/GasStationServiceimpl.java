5cb1bcaff41e3526633ddba2f53542a4930c504b
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2020-05-15 15:17:36 +0200

    feat:Implemented methods in the GasStationServiceImpl class(getGasStationsByProximity, getGasStationsWithCoordinates, getGasStationsWithoutCoordinates, getGasStationByCarSharing

--- src/main/java/it/polito/ezgas/service/impl/GasStationServiceimpl.java
+++ src/main/java/it/polito/ezgas/service/impl/GasStationServiceimpl.java
@@ -1,6 +1,7 @@
 package it.polito.ezgas.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import exception.PriceException;
 import it.polito.ezgas.dto.GasStationDto;
 import it.polito.ezgas.repository.GasStationRepository;
 import it.polito.ezgas.service.GasStationService;
+import it.polito.ezgas.utils.Haversine;
 
 /**
  * Created by softeng on 27/4/2020.
@@ -55,22 +57,96 @@ public class GasStationServiceimpl implements GasStationService {
 
 	@Override
 	public List<GasStationDto> getGasStationsByProximity(double lat, double lon) throws GPSDataException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return getAllGasStations().stream()
+			.filter( (g) -> Haversine.distance(lat, lon, g.getLat(), g.getLon() ) < 1)
+			.collect(Collectors.toList());
+			}
 
 	@Override
 	public List<GasStationDto> getGasStationsWithCoordinates(double lat, double lon, String gasolinetype,
 			String carsharing) throws InvalidGasTypeException, GPSDataException {
-		// TODO Auto-generated method stub
-		return null;
+		List<GasStationDto> gs = getAllGasStations().stream()
+				.filter( (g) -> Haversine.distance(lat, lon, g.getLat(), g.getLon() ) < 1)
+				.collect(Collectors.toList());
+		if(carsharing != null) {
+			gs = gs.stream()
+					.filter( (g) -> g.getCarSharing().equals(carsharing))
+					.collect(Collectors.toList());
+		}
+		switch(gasolinetype) {
+		  case "diesel":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasDiesel())
+				.collect(Collectors.toList());
+		    break;
+		  case "methane":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasMethane())
+				.collect(Collectors.toList());
+		    break;
+		  case "gas":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasGas())
+				.collect(Collectors.toList());
+		    break;
+		  case "super":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasSuper())
+				.collect(Collectors.toList());
+		    break;
+		  case "superplus":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasSuperPlus())
+				.collect(Collectors.toList());
+		    break;
+		  default:
+			  if(gasolinetype != null)
+			  throw new InvalidGasTypeException("Gas Type not supported");
+		}
+		return gs;
 	}
 
 	@Override
 	public List<GasStationDto> getGasStationsWithoutCoordinates(String gasolinetype, String carsharing)
 			throws InvalidGasTypeException {
-		// TODO Auto-generated method stub
-		return null;
+		//there is to method description so i dont know if it does what it needs
+		List<GasStationDto> gs = getAllGasStations();
+		if(carsharing != null) {
+			gs = gs.stream()
+					.filter( (g) -> g.getCarSharing().equals(carsharing))
+					.collect(Collectors.toList());
+		}
+		switch(gasolinetype) {
+		  case "diesel":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasDiesel())
+				.collect(Collectors.toList());
+		    break;
+		  case "methane":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasMethane())
+				.collect(Collectors.toList());
+		    break;
+		  case "gas":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasGas())
+				.collect(Collectors.toList());
+		    break;
+		  case "super":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasSuper())
+				.collect(Collectors.toList());
+		    break;
+		  case "superplus":
+			  gs = gs.stream()
+				.filter( (g) -> g.getHasSuperPlus())
+				.collect(Collectors.toList());
+		    break;
+		  default:
+			  if(gasolinetype != null)
+			  throw new InvalidGasTypeException("Gas Type not supported");
+		}
+		return gs;
 	}
 
 	@Override
@@ -83,8 +159,9 @@ public class GasStationServiceimpl implements GasStationService {
 
 	@Override
 	public List<GasStationDto> getGasStationByCarSharing(String carSharing) {
-		// TODO Auto-generated method stub
-		return null;
+		return getAllGasStations().stream()
+				.filter( (g) -> g.getCarSharing().equals(carSharing))
+				.collect(Collectors.toList());
 	}
 	
 	
