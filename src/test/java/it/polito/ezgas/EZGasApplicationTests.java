5831614aba3afdd5566a31027fc658db64698c5a
s274475 <s274475@studenti.polito.it>
2020-05-17 18:50:30 +0000

    Junit added for getGasStationName method

--- src/test/java/it/polito/ezgas/EZGasApplicationTests.java
+++ src/test/java/it/polito/ezgas/EZGasApplicationTests.java
@@ -5,22 +5,23 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import it.polito.ezgas.service.impl.UserServiceimplTest;
+import it.polito.ezgas.dto.GasStationDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EZGasApplicationTests {
-	
-	public class UserServiceimplTest {
-		
-		@Test
-		public void testGetUserById() {
-			UserServiceimplTest GU = new UserServiceimplTest();
-			AssertTrue(userDto,GU.GetUserById(-1));
-		}
 
-	}
-	
+		
+	@Test
+    public void testgetGasStationName() {
+		GasStationDto GasStation = new GasStationDto();
+		String GasStationName = "test";
+		GasStation.setGasStationName(GasStationName);
+		assert(GasStation.getGasStationName() == GasStationName);
+    }
+
+
+
 	@Test
 	public void contextLoads() {
 	}
