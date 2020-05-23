ea742875e2485b441522f2aac0c4520e384b659b
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-24 00:10:30 +0200

    feat:adding a test to verify that a user who does not exist can not be deleted

--- src/test/java/it/polito/ezgas/service/impl/UserServiceImplStep2Tests.java
+++ src/test/java/it/polito/ezgas/service/impl/UserServiceImplStep2Tests.java
@@ -273,4 +273,73 @@ public class UserServiceImplStep2Tests {
 		assertTrue(userServiceImp.deleteUser(2));
 	}
 	
+	//Delete a User that does not exist
+	@Test
+	public void testdeleteUserDoesNotExist() throws InvalidUserException {
+		UserDto user1 = new UserDto(1, "Luca Oddone", "Password", "lucaoddone@polito.it", 3);
+		UserDto user2 = new UserDto(2, "Paola Oddone", "Password", "paolaoddone@polito.it", 4);
+		
+		ids.clear();
+		listUsers.clear();
+		listUsersDto.clear();
+		
+		when(userServiceImp.saveUser(user1)).thenAnswer( invocation -> {
+			if(ids.contains(1)==true)
+				return null;
+			User entity1 = new User("Luca Oddone", "Password", "lucaoddone@polito.it", 3);
+			entity1.setUserId(1);
+			when(userConverter.toUser(user1)).thenReturn(entity1);
+			listUsers.add(0,userConverter.toUser(user1));
+			listUsersDto.add(0,user1);
+			ids.add(0,1);
+			return user1;
+		});
+		
+		when(userServiceImp.saveUser(user2)).thenAnswer( invocation -> {
+			if(ids.contains(2)==true)
+				return null;
+			User entity2 = new User("Paola Oddone", "Password", "paolaoddone@polito.it", 4);
+			entity2.setUserId(2);
+			when(userConverter.toUser(user2)).thenReturn(entity2);
+			listUsers.add(1,userConverter.toUser(user2));
+			listUsersDto.add(1,user2);
+			ids.add(1,2);
+			return user2;
+		});
+		
+		assertEquals(1,userServiceImp.saveUser(user1).getUserId());
+		assertEquals(2,userServiceImp.saveUser(user2).getUserId());
+		
+		when(userServiceImp.deleteUser(1)).thenAnswer(invocation -> {
+			if(ids.contains(1)) {
+				ids.remove(0);
+				listUsers.remove(0);
+				listUsersDto.remove(0);
+				return true;
+			}
+			return null;
+		});
+		
+		when(userServiceImp.deleteUser(2)).thenAnswer(invocation -> {
+			if(ids.contains(2) && ids.contains(1)) {
+				ids.remove(1);
+				listUsers.remove(1);
+				listUsersDto.remove(1);
+				return true;
+			}
+			if(ids.contains(2) && ids.contains(1)==false) {
+				ids.remove(0);
+				listUsers.remove(0);
+				listUsersDto.remove(0);
+				return true;
+			}
+			return null;
+		});
+		
+		assertTrue(userServiceImp.deleteUser(1));
+		assertTrue(userServiceImp.deleteUser(2));
+		
+		assertNull(userServiceImp.deleteUser(1));
+		assertNull(userServiceImp.deleteUser(2));
+	}
 }
