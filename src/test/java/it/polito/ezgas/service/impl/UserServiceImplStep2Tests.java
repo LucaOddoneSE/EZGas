3b7cd372c60e39e7ca539fbb29635df1bcc5e0e5
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-23 23:58:17 +0200

    feat:adding test for checking the correct retrieving of an empty DB

---
+++ src/test/java/it/polito/ezgas/service/impl/UserServiceImplStep2Tests.java
@@ -0,0 +1,40 @@
+package it.polito.ezgas.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import exception.InvalidLoginDataException;
+import exception.InvalidUserException;
+import it.polito.ezgas.converter.UserConverter;
+import it.polito.ezgas.dto.IdPw;
+import it.polito.ezgas.dto.LoginDto;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+
+public class UserServiceImplStep2Tests {
+
+	private UserServiceimpl userServiceImp = mock(UserServiceimpl.class);
+
+	private UserConverter userConverter = mock(UserConverter.class);
+	
+	private List<User> listUsers = new ArrayList<>();
+	private List<UserDto> listUsersDto = new ArrayList<>();
+	private List<Integer> ids = new ArrayList<>();
+	
+	// Empty DB
+	@Test
+	public void testgetAllUsersEmptyDB() {
+		ids.clear();
+		listUsers.clear();
+		listUsersDto.clear();
+		
+		when(userServiceImp.getAllUsers()).thenReturn(listUsersDto);
+		assertEquals(0,userServiceImp.getAllUsers().size());
+	}
+}
