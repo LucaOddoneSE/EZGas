fcace55d6aad667e94a097a436e3db34a8a192d3
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-23 16:47:43 +0200

    feat:adding UserServiceImplStep1 class to the Test Suite Case

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -5,12 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import it.polito.ezgas.entity.UserTest;
+import it.polito.ezgas.service.impl.UserServiceImplStep1;
 import it.polito.ezgas.dto.IdPwTest;
 import it.polito.ezgas.dto.LoginDtoTest;
 import it.polito.ezgas.entity.GasStationTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, LoginDtoTest.class })
+@SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, LoginDtoTest.class, UserServiceImplStep1.class })
 public class AllTests {
 
 }
