86cfb8af07ec5d3bac666906ca9a6477d8ed5a63
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-24 21:42:18 +0200

    feat:adding UserConverterTests and GasStationImplStep3 classes to the suite case

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -7,6 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 import it.polito.ezgas.entity.UserTest;
 import it.polito.ezgas.service.impl.UserServiceImplStepN;
 import it.polito.ezgas.service.impl.UserServiceImplStep2Tests;
+import it.polito.ezgas.service.impl.UserServiceImplStep3Tests;
+import it.polito.ezgas.converter.UserConverterTests;
 import it.polito.ezgas.dto.IdPwTest;
 import it.polito.ezgas.dto.LoginDtoTest;
 import it.polito.ezgas.entity.GasStationTest;
@@ -14,7 +16,7 @@ import it.polito.ezgas.entity.GasStationTest;
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, 
 	LoginDtoTest.class, UserServiceImplStepN.class, UserServiceImplStep2Tests.class,
-	UserServiceImplStepN.class})
+	UserServiceImplStep3Tests.class, UserConverterTests.class})
 public class AllTests {
 
 }
