263fdacbedd6dde6631dc5e0a3dde907323f0d3b
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-24 18:26:33 +0200

    feat:adding a new test suite

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import it.polito.ezgas.entity.UserTest;
-import it.polito.ezgas.service.impl.UserServiceImplStep1;
+import it.polito.ezgas.service.impl.UserServiceImplStepN;
 import it.polito.ezgas.service.impl.UserServiceImplStep2Tests;
 import it.polito.ezgas.dto.IdPwTest;
 import it.polito.ezgas.dto.LoginDtoTest;
@@ -13,7 +13,8 @@ import it.polito.ezgas.entity.GasStationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, 
-	LoginDtoTest.class, UserServiceImplStep1.class, UserServiceImplStep2Tests.class })
+	LoginDtoTest.class, UserServiceImplStepN.class, UserServiceImplStep2Tests.class,
+	UserServiceImplStepN.class})
 public class AllTests {
 
 }
