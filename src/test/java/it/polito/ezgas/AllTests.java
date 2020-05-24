a71f657616d8e8e679d45eeaeae71f0c8ad049dd
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-24 23:55:19 +0200

    feat:adding UserRepositoryTests to the Tests suite case

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import it.polito.ezgas.entity.UserTest;
+import it.polito.ezgas.repository.UserRepositoryTests;
 import it.polito.ezgas.service.impl.UserServiceImplStepN;
 import it.polito.ezgas.service.impl.UserServiceImplStep2Tests;
 import it.polito.ezgas.service.impl.UserServiceImplStep3Tests;
@@ -16,7 +17,7 @@ import it.polito.ezgas.entity.GasStationTest;
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, 
 	LoginDtoTest.class, UserServiceImplStepN.class, UserServiceImplStep2Tests.class,
-	UserServiceImplStep3Tests.class, UserConverterTests.class})
+	UserServiceImplStep3Tests.class, UserConverterTests.class, UserRepositoryTests.class})
 public class AllTests {
 
 }
