73de5635121d4621b0ec55ea3cd162cd81110d92
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-26 11:17:58 +0200

    feat:adding GasStationServiceimplTests class to the suite test cases

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import it.polito.ezgas.entity.UserTest;
 import it.polito.ezgas.repository.UserRepositoryTests;
 import it.polito.ezgas.service.impl.UserServiceImplStepN;
+import it.polito.ezgas.service.impl.GasStationServiceImplTests;
 import it.polito.ezgas.service.impl.UserServiceImplStep2Tests;
 import it.polito.ezgas.service.impl.UserServiceImplStep3Tests;
 import it.polito.ezgas.converter.UserConverterTests;
@@ -17,7 +18,8 @@ import it.polito.ezgas.entity.GasStationTest;
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, 
 	LoginDtoTest.class, UserServiceImplStepN.class, UserServiceImplStep2Tests.class,
-	UserServiceImplStep3Tests.class, UserConverterTests.class, UserRepositoryTests.class})
+	UserServiceImplStep3Tests.class, UserConverterTests.class, UserRepositoryTests.class,
+	GasStationServiceImplTests.class})
 public class AllTests {
 
 }
