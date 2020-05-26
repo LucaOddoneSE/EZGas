122f8525bcc74bb9af312f23c26e31cfe94016aa
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-26 19:21:56 +0200

    fix:fixing errors in AllTests.java file

--- src/test/java/it/polito/ezgas/AllTests.java
+++ src/test/java/it/polito/ezgas/AllTests.java
@@ -7,9 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 import it.polito.ezgas.entity.UserTest;
 import it.polito.ezgas.repository.UserRepositoryTests;
 import it.polito.ezgas.service.impl.UserServiceImplStepN;
+import it.polito.ezgas.service.impl.UserServiceImplTests;
 import it.polito.ezgas.service.impl.GasStationServiceImplTests;
-import it.polito.ezgas.service.impl.UserServiceImplStep2Tests;
-import it.polito.ezgas.service.impl.UserServiceImplStep3Tests;
 import it.polito.ezgas.converter.GasStationConverterTests;
 import it.polito.ezgas.converter.UserConverterTests;
 import it.polito.ezgas.dto.IdPwTest;
@@ -18,8 +17,8 @@ import it.polito.ezgas.entity.GasStationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, GasStationTest.class, IdPwTest.class, 
-	LoginDtoTest.class, UserServiceImplStepN.class, UserServiceImplStep2Tests.class,
-	UserServiceImplStep3Tests.class, UserConverterTests.class, UserRepositoryTests.class,
+	LoginDtoTest.class, UserServiceImplStepN.class,
+	UserServiceImplTests.class, UserConverterTests.class, UserRepositoryTests.class,
 	GasStationServiceImplTests.class, GasStationConverterTests.class})
 public class AllTests {
 
