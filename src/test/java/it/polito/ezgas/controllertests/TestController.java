6a9a9a720434d6ef1cd5826e9741fe42a19e138b
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-05-30 14:55:50 +0200

    feat:adding REST API Test for getAllUsers() method

--- src/test/java/it/polito/ezgas/controllertests/TestController.java
+++ src/test/java/it/polito/ezgas/controllertests/TestController.java
@@ -1,19 +1,25 @@
 package it.polito.ezgas.controllertests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.polito.ezgas.dto.UserDto;
+
 public class TestController {
 	
+	//UserController
 	@Test
 	public void testGetUserById() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/user/getUser/2");
@@ -23,5 +29,20 @@ public class TestController {
 		assertTrue(response.getStatusLine().getStatusCode() == 200);
 		assertTrue(jsonFromResponse.contains("lucaoddone@polito.it"));
 	}
-
+	
+	/*@Test
+	public void testSaveUser() throws ClientProtocolException, IOException {
+	} */
+	@Test
+	public void testGetAllUsers() throws ClientProtocolException, IOException {
+		HttpUriRequest request = new HttpGet("http://localhost:8080/user/getAllUsers");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+		
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		UserDto[] userDtoArray = mapper.readValue(jsonFromResponse, UserDto[].class);
+		
+		assertTrue(userDtoArray.length == 4);
+	}
+	
 }
