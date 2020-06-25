86ca03258063d2addabaac54c6865cfa3bc001be
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-06-25 16:32:12 +0200

    fix:fixing Date format

--- src/main/java/it/polito/ezgas/utils/Day.java
+++ src/main/java/it/polito/ezgas/utils/Day.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class Day {
 	
 	public static int calculateDays(String s) throws ParseException {
-		Date date =  new SimpleDateFormat("MM-dd-YYYY").parse(s);
+		Date date =  new SimpleDateFormat("MM-dd-yyyy").parse(s);
 		Calendar c1 = Calendar.getInstance();
 		Calendar c2 = Calendar.getInstance();
 		c2.setTime(date);
@@ -21,7 +21,7 @@ public class Day {
 	
 	public static String calendarToString() {
 		Date date = Calendar.getInstance().getTime();
-		DateFormat dateFormat = new SimpleDateFormat("MM-dd-YYYY");
+		DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
 		return dateFormat.format(date);
 	}
 }
