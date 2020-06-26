120d82a45d1d2194b10ba61c73963342c004e410
LUCA ODDONE s204306 <s204306@studenti.polito.it>
2020-06-26 10:41:55 +0200

    fix:fixing integration tests related to GasStationConverter accordingli with the new attribute PremiumDiesel

--- src/test/java/it/polito/ezgas/converter/GasStationConverterTests.java
+++ src/test/java/it/polito/ezgas/converter/GasStationConverterTests.java
@@ -14,8 +14,9 @@ public class GasStationConverterTests {
 	
 	@Test
 	public void testToGasStationDto() {
-		gasStation = new GasStation("GasStation1","Via Italia 1",true,true,false,false,true,
-				"BlaBlaCar",81.574,111.320,1.25,1.55,0,0,0.90,null,null,0);
+		gasStation = new GasStation("GasStation1","Via Italia 1",true,true,false,false,true,true,
+				"BlaBlaCar",81.574,111.320,(double) 1.25,(double) 1.55,(double) 0,(double) 0,(double) 0.90,
+				(double) 1.45,null,null,0);
 		
 		gasStation.setGasStationId(1);
 		
@@ -29,8 +30,9 @@ public class GasStationConverterTests {
 	
 	@Test
 	public void testToGasStation() {
-		gasStationDto = new GasStationDto(1,"GasStation1","Via Italia 1",true,true,false,false,true,
-				"BlaBlaCar",81.574,111.320,1.25,1.55,0,0,0.90,null,null,0);
+		gasStationDto = new GasStationDto(1,"GasStation1","Via Italia 1",true,true,false,false,true,true,
+				"BlaBlaCar",81.574,111.320,(double) 1.25,(double) 1.55,(double) 0,(double) 0,
+				(double) 0.90,(double) 1.45,null,null,0);
 		
 		gasStation = gasStationConverter.toGasStation(gasStationDto);
 		
